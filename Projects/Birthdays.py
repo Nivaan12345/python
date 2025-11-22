c7668ef832fa1664465ccc07f4417414ff118163
Nivaan12345 <nivaanjangla@gmail.com>
2025-11-22 16:38:46 +0530

    Averages

---
+++ Projects/Birthdays.py
@@ -0,0 +1,11 @@
+print("hello")
+a="10/10/1982"
+b="19/1/1982"
+c="20/5/1953"
+d="19/2/1952"
+e="29/7/2014"
+print("Meena's birthday is",c)
+print("Neil's birthday is",b)
+print("Pooja's birthday is",a)
+print("Nitin's birthday is",d)
+print("Nivaan's birthday is",e)
