67fb79f1d2bbb3376757c5043dba50c4a5578943
Nivaan12345 <nivaanjangla@gmail.com>
2025-11-22 17:36:48 +0530

    birth date

--- Projects/Birthdays.py
+++ Projects/Birthdays.py
@@ -4,8 +4,8 @@ b="19/1/1982"
 c="20/5/1953"
 d="19/2/1952"
 e="29/7/2014"
-print("Meena's birthday is",c)
-print("Neil's birthday is",b)
-print("Pooja's birthday is",a)
-print("Nitin's birthday is",d)
-print("Nivaan's birthday is",e)
+print("Meena's birth date is",c)
+print("Neil's birth date is",b)
+print("Pooja's birth date is",a)
+print("Nitin's birth date is",d)
+print("Nivaan's birth date is",e)
