9d1ebe0a27ea94be8e7f52dbaf0aed13972ca699
Nivaan12345 <nivaanjangla@gmail.com>
2025-11-29 16:51:17 +0530

    BMI checker

---
+++ Logical operators/BMI checker.py
@@ -0,0 +1,16 @@
+height=float(input("Enter your height in cm's "))
+weight=float(input("Enter your weight in kg's "))
+BMI=weight/(height/100)**2
+if(BMI<=18.4):
+    print("You are under weight")
+elif(BMI<=24.9):
+    print("You are healthy")
+elif(BMI<=29.9):
+    print("You are over weight")
+elif(BMI<=34.9):
+    print("You are severely over weight")
+elif(BMI<=39.9):
+    print("you are obese")
+else:
+    print("You are severely obese")
+#end
