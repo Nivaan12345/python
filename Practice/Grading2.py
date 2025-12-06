5e14ef87e80cfdffca2bdd43077ed6249e85be87
Nivaan12345 <nivaanjangla@gmail.com>
2025-12-06 16:18:21 +0530

    grading

---
+++ Practice/Grading2.py
@@ -0,0 +1,15 @@
+a=int(input("Give me your marks for English "))
+b=int(input("Give me your marks for Maths "))
+c=int(input("Give me your marks for Science "))
+d=int(input("Give me your marks for History "))
+e=int(input("Give me your marks for Geography "))
+avg=((a+b+c+d+e)/5)
+if(avg<=25):
+    x=("D")
+elif(avg<=50 and avg>=25):
+    x=("C")
+elif(avg<=75 and avg>=50):
+    x=("B")
+else:
+    x=("A")
+print("You have got grade",x,"with a percentage of",avg,"%")
