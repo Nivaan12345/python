ab9d897a1746950114d5922086069ae5f9a09eff
Nivaan12345 <nivaanjangla@gmail.com>
2025-12-10 08:07:35 +0530

    number

---
+++ Number swapping.py
@@ -0,0 +1,4 @@
+a=(input("Enter a number to swap "))
+b=(input("Enter a second number to swap "))
+c=(input("Enter a third number to swap "))
+print(c,b,a)
---
+++ Other/mumma's 44'th birthday.py
@@ -0,0 +1,14 @@
+p=str(input("what is the password to open this encrypted file "))
+if(p==str("love you" or "Love you")):
+    turf=int(input("enter the price for turf per sq.ft "))
+    sq=int(input("Enter the amount of sq.ft you want to turf"))
+    tusq=(turf*sq)
+    ext=int(input("Add the amount of any extra charges "))
+    tsqe=(tusq+ext)
+    gst=(input("Enter the percentage of gst applied"))
+    gtqe=((gst/100)*tsqe)
+    rtge=(tsqe-gtqe)
+    print("Your Quotatation should be of Rupees",rtge)
+else:
+    print("Incorrect password ,please try again later")
+#end
