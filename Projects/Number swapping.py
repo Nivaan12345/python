fd85d9da691e0f2d5f59eda2e0884489d8aeb3f5
Nivaan12345 <nivaanjangla@gmail.com>
2025-12-12 22:11:25 +0530

    number swapping

--- Projects/Number swapping.py
+++ Projects/Number swapping.py
@@ -1,4 +1,8 @@
-a=(input("Enter a number to swap "))
-b=(input("Enter a second number to swap "))
-c=(input("Enter a third number to swap "))
-print(c,b,a)
+a=(input("Enter a number to swap(a) "))
+b=(input("Enter a second number to swap(b) "))
+c=(input("Enter a third number to swap(c) "))
+temp=a
+a=b
+b=c
+c=temp
+print("a,b and c are now",a,",",b,",",c)
