ed4484c2c1c6bf06d3700eeed0e487634cd10519
Nivaan12345 <nivaanjangla@gmail.com>
2025-12-12 22:44:01 +0530

    exameligiblity

--- nested conditional statements/exam eligiblity check.py
+++ nested conditional statements/exam eligiblity check.py
@@ -1,10 +1,12 @@
 medical=str(input("Do you have a medical cause('Y' for yes and 'N' for no) "))
-if(medical=='N' or'n'):
+if(medical==str('N' or 'n')):
     attend=int(input("How much is your attendance "))
     if(attend<=75):
         print("you are not allowed in the exam")
     else:
         print("You are allowed in the exam")
-else:
+elif(medical==str('y' or 'Y')):
     print("You are allowed in the exam")
+else:
+    print("Invalid, please try again")
 #en
