39f9381841faa42a4e40c0c69a43cb4fc32c2a83
Nivaan12345 <nivaanjangla@gmail.com>
2025-12-13 16:04:48 +0530

    electricity bill

--- nested conditional statements/electricity bill.py
+++ nested conditional statements/electricity bill.py
@@ -1,9 +1,9 @@
 units=int(input("Enter the number of units for your electricity bill "))
-if(units<50):
+if(units<=50):
     x=((units*2.60)+25)
-elif(units>50 and units<100):
+elif(units>=50 and units<100):
     x=((units*3.25)+35)
-elif(units>100 and units<200):
+elif(units>=100 and units<200):
     x=((units*5.26)+45)
 else:
     x=((units*8.45)+75)
