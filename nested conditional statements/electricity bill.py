56c9726e2db576142ec0c444f183a21042d573a4
Nivaan12345 <nivaanjangla@gmail.com>
2025-12-13 16:05:05 +0530

    electricity bill

--- nested conditional statements/electricity bill.py
+++ nested conditional statements/electricity bill.py
@@ -3,7 +3,7 @@ if(units<=50):
     x=((units*2.60)+25)
 elif(units>=50 and units<100):
     x=((units*3.25)+35)
-elif(units>=100 and units<200):
+elif(units>=100 and units<=200):
     x=((units*5.26)+45)
 else:
     x=((units*8.45)+75)
