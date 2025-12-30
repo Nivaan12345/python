36820e1902e6b60ccb930a73241ecc8a7a50b95f
Nivaan12345 <nivaanjangla@gmail.com>
2025-12-30 15:45:07 +0530

    floyd triangle

---
+++ Patterns/Floyd triangle.py
@@ -0,0 +1,7 @@
+rows=int(input("Enter the number of rows you want the triangle to be "))
+num=1
+for i in range(1,rows+1,1):
+    for j in range(1,i+1,1):
+        print(num,end='')
+        num=num+1
+    print()
