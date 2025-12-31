a84a603d6c0e1b4352caead0b1630f76c7075c24
Nivaan12345 <nivaanjangla@gmail.com>
2025-12-31 15:57:28 +0530

    turtle

---
+++ Practice/star.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.Screen().bgcolor("green")
+turtle.Screen().setup(300,400)
+star=turtle.Turtle()
+angle=360/3
+for i in range(5):
+    star.forward(100)
+    star.right(144)
+    
---
+++ Turtle/spiral.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.Screen().bgcolor("green")
+turtle.Screen().setup(300,400)
+spiral=turtle.Turtle()
+size=0
+while True:
+    for i in range(4):
+        spiral.forward(size+1)
+        spiral.left(90)
+        size=size-5
+    size=size+1
+turtle.done()
