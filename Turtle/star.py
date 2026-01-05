beca7bcf1846086f0c0d739d8b56c581936e3d09
Nivaan12345 <nivaanjangla@gmail.com>
2026-01-05 19:34:36 +0530

    star

--- Turtle/star.py
+++ Turtle/star.py
@@ -3,7 +3,7 @@ turtle.Screen().bgcolor("green")
 turtle.Screen().setup(300,400)
 star=turtle.Turtle()
 angle=360/3
-for i in range(5):
+for i in range(4):
     star.forward(100)
     star.right(144)
-    
+star.forward(100)
