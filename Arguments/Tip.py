f295f9c0b2771d719009f7223beb35c8bd1852d0
Nivaan12345 <nivaanjangla@gmail.com>
2026-01-07 18:32:42 +0530

    tip

---
+++ Arguments/Tip.py
@@ -0,0 +1,6 @@
+bill=int(input("Enter the total bill amount "))
+tip_perc=int(input("How much percentage of the bill do you want to tip "))
+def total_calc(bill,tip_perc):
+    total=(((tip_perc/100)*bill)+bill)
+    print("The total amount you have to pay is Rupees",total)
+total_calc(bill,tip_perc)
