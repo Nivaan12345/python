a5465457a407da130c10052f6b5ca08d009594f9
Nivaan12345 <nivaanjangla@gmail.com>
2026-01-24 16:45:16 +0530

    random date and time

--- Python Functions (Module 3)/Date,time and calender modules/Random date.py
+++ Python Functions (Module 3)/Date,time and calender modules/Random date.py
@@ -2,6 +2,6 @@ import random
 import datetime
 start=datetime.date(2026,1,1)
 end=datetime.date(2026,12,31)
-randate=random.randint(0,(end - start).days)
-randays=start+datetime.timedelta(days=randate)
+randays=random.randint(0,(end - start).days)
+randate=start+datetime.timedelta(days=randays)
 print("The random date is:",randate)
