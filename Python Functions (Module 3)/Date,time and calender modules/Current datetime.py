199b5adb1e79c7286593f3aaa4f25812c7212800
Nivaan12345 <nivaanjangla@gmail.com>
2026-01-24 16:27:29 +0530

    current date time

--- Python Functions (Module 3)/Date,time and calender modules/Current datetime.py
+++ Python Functions (Module 3)/Date,time and calender modules/Current datetime.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import calendar
-x=int(input("Enter the year for which you want the calendar for"))
-y=int(input("Enter the month for which you want the calendar for"))
+x=int(input("Enter the year for which you want the calendar for "))
+y=int(input("Enter the month for which you want the calendar for "))
 print(datetime.now())
-print(calendar(x,y))
+print(calendar.month(x,y))
