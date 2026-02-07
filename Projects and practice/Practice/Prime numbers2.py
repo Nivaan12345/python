91ae355c0a1ba1c1a8f2de602d47a4f4be1a7352
Nivaan12345 <nivaanjangla@gmail.com>
2026-02-07 15:04:27 +0530

    x

--- Projects and practice/Practice/Prime numbers2.py
+++ Projects and practice/Practice/Prime numbers2.py
@@ -1,5 +1,5 @@
 x=int(input("Enter the number you want to check if is a prime"))
-y=x-1
-while True:
-    if x%y==0:
-        print
+if x%2==0 or x%3==0 or x%5==0 or x%7==0 or x<1:
+    print(x," is not a prime number")
+else:
+    print(x,"is a prime number")
