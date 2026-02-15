73f774d2ee430c73c7e913c822c83f291fad963c
Nivaan12345 <nivaanjangla@gmail.com>
2026-02-15 16:04:30 +0530

    dictionary

---
+++ Projects and practice/Practice/Dictionary3.py
@@ -0,0 +1,7 @@
+x={'Pooja' and 'pooja':9819030887,'Neil' and 'neil':9867745778,'Meena' and'meena':9768145778,
+'Rupa' and 'rupa':7021195095,'Prakash' and 'prakash':9004037700}
+x['Home']=9867333557
+y=(input("Enter the name you want to find the number of: "))
+print(x[y])
+print(x)
+#end
