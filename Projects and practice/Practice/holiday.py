97f4825929615d9f09e54bf368cad3fdbff921c0
Nivaan12345 <nivaanjangla@gmail.com>
2026-02-15 22:00:28 +0530

    holiday

--- Projects and practice/Practice/holiday.py
+++ Projects and practice/Practice/holiday.py
@@ -1,4 +1,4 @@
-holiday=[2/5/2026,]
+holiday=[19/2/2026,3/3/2026,]
 x=(input("Enter the day you want to check is a holiday or not in the format of day/month/year: "))
 for i in holiday:
     if x==i:
