be777dfcd8198cba4102d0b5068e341d5eb854e4
Nivaan12345 <nivaanjangla@gmail.com>
2026-02-24 18:13:58 +0530

    mean

---
+++ Projects and practice/Practice/mean.py
@@ -0,0 +1,10 @@
+l=int(input("Enter the amount of numbers you want to add "))
+x=[]
+for m in range(l):
+    temp=int(input("Enter the num "))
+    x.append(temp)
+y=0
+for i in x:
+    y=y+i
+z=y/l
+print("The mean of your numbers is",z)
