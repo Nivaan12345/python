865224f5c26b9fe46ddc1a7192a685d2d994f624
Nivaan12345 <nivaanjangla@gmail.com>
2026-03-02 18:38:37 +0530

    s

---
+++ Projects and practice/Projects/Module 4/Random password.py
@@ -0,0 +1,25 @@
+import random
+x=int(input("How many characters  should be in the password "))
+f=''
+lower='qwertyuiopasdfghjklzxcvbnm'
+upper='QWERTYUIOPASDFGHJKLZXCVBNM'
+num='1234567890'
+def berry(x,f,):
+    for v in range(x+1):
+        v=random.choice(lower,upper,num)
+        f=f+v
+        
+y=berry(x,f)
+if int in y:
+    if (str.upper) in y:
+        if (str.lower) in y:
+            print(f)
+        else:
+            f=''
+            y
+    else:
+        f=''
+        y
+else:
+    f=''
+    y
