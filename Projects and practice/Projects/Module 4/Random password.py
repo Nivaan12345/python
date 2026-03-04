857b0223d06191a97520287edde38c1b565d5022
Nivaan12345 <nivaanjangla@gmail.com>
2026-03-04 22:31:32 +0530

    trh

--- Projects and practice/Projects/Module 4/Random password.py
+++ Projects and practice/Projects/Module 4/Random password.py
@@ -1,24 +1,17 @@
 import random
 x=int(input("How many characters  should be in the password "))
-f=''
-lower='qwertyuiopasdfghjklzxcvbnm'
-upper='QWERTYUIOPASDFGHJKLZXCVBNM'
-num='1234567890'
-def berry(x,f,):
-    for v in range(x+1):
-        v=random.choice(lower,upper,num)
-        f=f+v
-y=berry(x,f)
-if int in y:
-    if (str.upper) in y:
-        if (str.lower) in y:
-            print(f)
+y=(1,2)
+a=""
+r=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+def  berry(x):
+    g=random.choice(y)
+    for i in range(x+1):
+        if g==1:
+            temp=random.randint(0,9)
         else:
-            f=''
-            y
-    else:
-        f=''
-        y
-else:
-    f=''
-    y
+            temp=random.choice(r)
+    a=a+temp
+while int is not  a:
+    berry(x)
+print(a)
