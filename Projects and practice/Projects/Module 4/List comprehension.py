a85f0a39bc77a9a46d28b6cf20a6674a629d9035
Nivaan12345 <nivaanjangla@gmail.com>
2026-03-07 17:24:48 +0530

    t

--- Projects and practice/Projects/Module 4/List comprehension.py
+++ Projects and practice/Projects/Module 4/List comprehension.py
@@ -1,7 +1,19 @@
 a=int(input("Enter a number "))
-x=[]
-for i in range(a+1):
-    if i%2!=0:
-        x.append(i)
+x=[1,3,5,7,9,11,13,15,17,19,21]
+temp=[]
+for i in range(a):
+    if i in x:
+        temp.append(i)
     else:
         continue
+print(x)
+print(temp)
+x=[]
+for j in temp:
+    x.append(j)
+
+
+f=['apple','banana','orange','grape','pear','mango','pomogranate','litchi']
+for i in f:
+    str.upper(i[0])
+print(f)
