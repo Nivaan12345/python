9df98844c4b5f78923d9698f2849a0c5fe542707
Nivaan12345 <nivaanjangla@gmail.com>
2026-03-19 16:14:31 +0530

    BRT

--- Projects and practice/Projects/Module 5/circle.py
+++ Projects and practice/Projects/Module 5/circle.py
@@ -1,1 +1,13 @@
+class circle:
+    def __init__(self,r):
+        self.r=r
+    def area(r):
+        area=(22/7)*r*r
+        print(f"the area of your circle is {area}")
+    def circum(r):
+        circum=r*2*(22/7)
+        print("The circumference of your circle is",circum)
+circlex=circle(int(input("Enter the radius of a circle")))
+print(circlex.area())
+print(circlex.circum())
 #INCOMPLETE
