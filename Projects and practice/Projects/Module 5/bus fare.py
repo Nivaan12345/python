16cf7f54daebf1aa587dcfa4fafdf5030bc82148
Nivaan12345 <nivaanjangla@gmail.com>
2026-03-28 16:11:06 +0530

    bus

--- Projects and practice/Projects/Module 5/bus fare.py
+++ Projects and practice/Projects/Module 5/bus fare.py
@@ -1,14 +1,13 @@
 class vehicle:
     def __init__(self,seating):
         self.seating=seating
-        self.fare=seating*100
 class bus(vehicle):
     def __init__(self,maintanance,seating):
         self.maintanance=maintanance
 
         vehicle.__init__(self,seating)
-    def fare(maintanance,seating):
-        fare=((maintanance/100)*seating)+seating
-        print("the bus fare is",fare)
+    def fare(self,maintanance,seating):
+        res=((maintanance/100)*seating)+seating
+        print("the bus fare is",res)
 best=bus(10,50)
 best.fare(10,50)
