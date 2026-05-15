1f207fa3be13ef0f0e2ce9faec3d17d1bd221188
Nivaan12345 <nivaanjangla@gmail.com>
2026-05-15 17:34:40 +0530

    b4 rtg4

--- Projects and practice/Projects/Module 6/add sprites.py
+++ Projects and practice/Projects/Module 6/add sprites.py
@@ -9,24 +9,31 @@ x,y=250,250
 
 
 
-Back.fill((58,58,58))
-pygame.draw.rect(Back,(pygame.Color("darkgreen")),
-                 (x,y,60,60))
-pygame.draw.rect(Back,(pygame.Color("darkblue")),
-                 (320,320,60,60))
-pygame.display.flip()
 
+class Sprite(pygame.sprite.Sprite):
+
+    def __init__(self,color,height,width):
+
+        super().__init__()
+
+        self.image=pygame.Surface([width,height])
+        self.image.fill(color)
 done=False
 while not done:
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
-           done=True
-    pressed=pygame.key.get_pressed()
-    if pressed[pygame.K_LEFT]: x -= 3
-    if pressed[pygame.K_RIGHT]: x += 3
-    if pressed[pygame.K_UP]: y -= 3
-    if pressed[pygame.K_DOWN]: y += 3
-
+               done=True
+        pressed=pygame.key.get_pressed()
+        if pressed[pygame.K_LEFT]: x -= 10
+        if pressed[pygame.K_RIGHT]: x += 10
+        if pressed[pygame.K_UP]: y -= 10
+        if pressed[pygame.K_DOWN]: y += 10
+        Back.fill((58,58,58))       
+        pygame.draw.rect(Back,(pygame.Color("darkgreen")),
+                 (x,y,60,60))
+        pygame.draw.rect(Back,(pygame.Color("darkblue")),
+                 (320,320,60,60))
+        pygame.display.flip()
 clock.tick(90)
 
 pygame.quit()
