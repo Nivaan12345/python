6af9208681ed494ae7a61a54d01b24744d5ce99a
Nivaan12345 <nivaanjangla@gmail.com>
2026-05-16 12:26:50 +0530

    k86j7yhtrb

--- Projects and practice/Projects/Module 6/custom event.py
+++ Projects and practice/Projects/Module 6/custom event.py
@@ -9,28 +9,30 @@ x,y=250,250
 spwi,sphe=60,60
 clock=pygame.time.Clock()
 done=False
-
 x=min(max(0,x),scwi - spwi)
 y=min(max(0,y),sche - sphe)
+class sprite(pygame.sprite.Sprite):
 
-def colour_change(self):
-    self.image.fill(pygame.Color('orange'))
+    def colour_change(self):
+        self.image.fill(pygame.Color('orange'))
 
 
-screen.fill((255,0,0))
+    screen.fill((255,0,0))
 
-sp1=pygame.draw.rect(screen,pygame.Color('darkgreen'),
+    sp1=pygame.draw.rect(screen,pygame.Color('darkgreen'),
                          (x,y,spwi,sphe))
-sp2=pygame.draw.rect(screen,pygame.Color('darkblue'),
+    sp2=pygame.draw.rect(screen,pygame.Color('darkblue'),
                          (x,y,spwi,sphe))
+    
+    if sp1.bottom <=0 or sp1.top 
 
-pygame.display.flip()
-while not done:
-    for event in pygame.event.get():
-        if event.type== pygame.QUIT:
-            done=True
-        elif event.type==SPRITE_COLOUR_CHANGE_EVENT:
-            sp1.change_color()
+    
+    while not done:
+        for event in pygame.event.get():
+            if event.type== pygame.QUIT:
+                done=True
+            elif event.type==SPRITE_COLOUR_CHANGE_EVENT:
+                sp1.change_color()
 
 clock.tick(90)
 pygame.quit()
