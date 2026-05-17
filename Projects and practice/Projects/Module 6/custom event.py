db1f0bc67658bd77539e8b15895e123cb0b742ea
Nivaan12345 <nivaanjangla@gmail.com>
2026-05-17 13:23:18 +0530

    54er

--- Projects and practice/Projects/Module 6/custom event.py
+++ Projects and practice/Projects/Module 6/custom event.py
@@ -1,50 +1,45 @@
 import pygame
-
 pygame.init()
-scwi,sche=500,500
-screen=pygame.display.set_mode((scwi,sche))
-pygame.display.set_caption('custom event')
-screen.fill((255,255,255))
-SPRITE_COLOUR_CHANGE_EVENT=pygame.USEREVENT+2
-x,y=250,250
-spwi,sphe=60,60
-clock=pygame.time.Clock()
-done=False
-class sprite(pygame.sprite.Sprite):
 
-    def __init__(self):
+Back=pygame.display.set_mode((500,500))
+pygame.display.set_caption("Lets add sprites!!")
+clock=pygame.time.Clock()
+x,y=250,250
+SPRITE_COLOUR_CHANGE_EVENT=pygame.USEREVENT+1
+
+class Sprite(pygame.sprite.Sprite):
+
+    def __init__(self,color,height,width):
 
         super().__init__()
 
-    
-def change_color(self):
-    self.fill(pygame.Color("red"))
+        self.image=pygame.Surface([width,height])
+        self.image.fill(color)
+        self.image = pygame.Surface((50, 50)) 
 
-sp1=pygame.draw.rect(screen,pygame.Color('darkgreen'),
-                         (x,y,spwi,sphe))
-sp2=pygame.draw.rect(screen,pygame.Color('blue'),
-                         (400,400,spwi,sphe))
-
-
-    
-if sp1.bottom >=500 or sp1.top <0 or sp1.right >=500 or sp1.left <0:
-        pygame.event.post(pygame.event.Event(SPRITE_COLOUR_CHANGE_EVENT))
-
-
+done=False
 while not done:
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
-            done=True
+               done=True
         elif event.type==SPRITE_COLOUR_CHANGE_EVENT:
-            sp1.change_color()
+             sp1.image.fill(pygame.Color("red"))
+             (sp1.image.get_rect()).topleft=(100,10)
         pressed=pygame.key.get_pressed()
-        if pressed[pygame.K_LEFT]: x -= 3
-        if pressed[pygame.K_RIGHT]: x += 3
-        if pressed[pygame.K_UP]: y -= 3
-        if pressed[pygame.K_DOWN]: y += 3
-
-        x=min(max(0,x),scwi - spwi)
-        y=min(max(0,y),sche - sphe)
+        if pressed[pygame.K_LEFT]: x -= 10
+        if pressed[pygame.K_RIGHT]: x += 10
+        if pressed[pygame.K_UP]: y -= 10
+        if pressed[pygame.K_DOWN]: y += 10
+        Back.fill((58,58,58))       
+        sp1=pygame.draw.rect(Back,(pygame.Color("darkgreen")),
+                 (x,y,60,60))
+        pygame.draw.rect(Back,(pygame.Color("darkblue")),
+                 (320,320,60,60))
+        if sp1.bottom >=500 or sp1.top <0 or sp1.right >=500 or sp1.left <0:
+           pygame.event.post(pygame.event.Event(SPRITE_COLOUR_CHANGE_EVENT))
+        x=min(max(0,x),440)
+        y=min(max(0,y),440)
         pygame.display.flip()
 clock.tick(90)
+
 pygame.quit()
