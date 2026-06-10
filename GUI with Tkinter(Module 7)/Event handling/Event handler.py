c27116bd87af22e99fb977f2d784dd02881c5c3e
Nivaan12345 <nivaanjangla@gmail.com>
2026-06-10 18:29:24 +0530

    event

---
+++ GUI with Tkinter(Module 7)/Event handling/Event handler.py
@@ -0,0 +1,17 @@
+from tkinter import *
+w=Tk()
+
+w.title("Event handler")
+w.geometry("500x500")
+
+def event_keypress(event):
+    print(event.char)
+w.bind("<key>",event_keypress)
+
+def event_click(event):
+    print("This button has been clicked!")
+btn=Button(text="Click me!")
+btn.pack()
+
+btn.bind("<Button-1>",event_click)
+w.mainloop()
