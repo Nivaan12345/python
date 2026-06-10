0c1b01003726c458277bed95add4e38458bc12f0
Nivaan12345 <nivaanjangla@gmail.com>
2026-06-10 19:11:39 +0530

    length

---
+++ Projects and practice/Projects/Module 7/length calculator.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+w=Tk()
+w.title("Length Calculator app")
+w.geometry("500x500")
+
+lbl1=Label(text="Enter your length in inches",bg="#1A17D9")
+ent1=Entry()
+def display():
+    inch=int(ent1.get())
+    cm=inch*2.54
+    msg=(f"{inch} inches are {round(cm,2)} centimeters \n")
+    txtbx.insert(END,msg)
+txtbx=Text(bg="#118C5B",fg="black")
+btn=Button(text="Calculate the length in centimeters",command=display,bg="red")
+
+lbl1.place(x=20,y=20)
+ent1.place(x=265,y=20)
+btn.place(x=130,y=210)
+txtbx.place(x=0,y=250)
+w.mainloop()
+#end
