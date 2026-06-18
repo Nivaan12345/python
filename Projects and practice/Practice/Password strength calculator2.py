01bce57989df8bd96e3e9cb580482d1dbfa94899
Nivaan12345 <nivaanjangla@gmail.com>
2026-06-18 20:02:40 +0530

    poiuytre

--- Projects and practice/Practice/Password strength calculator2.py
+++ Projects and practice/Practice/Password strength calculator2.py
@@ -6,4 +6,22 @@ w.geometry("300x300")
 lbl1=Label(text="Enter your password",bg="light grey")
 ent1=Entry()
 def Calc():
+    r=0
+    upper=False
+    lower=False
+    number=False
+    symbol=False
     pas=ent1.get()
+    for i in pas:
+        if i==i.upper():
+            if upper==False:
+               r+=1
+               upper=True
+        elif i==i.lower():
+            if lower==False:
+                r+=1
+                lower=True
+        elif type(i)==int:
+            if number==False:
+                r+=1
+                number=True
